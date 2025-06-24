ed2940da7bd7125de05de3ad4321499e16053049
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2025-06-24 14:23:13 +0545

    today

---
+++ day6/intro.js
@@ -0,0 +1,7 @@
+console.log = "hello Rajan"
+console.log("name.length");
+console.log(name.trim());
+console.log("name.toUpperCase()");
+console.log("name.toLowerCase()");
+console.log(name.includes("Rajan"));
+console.log(name.replace("Rajan", "Rajan Pantha"));
---
+++ day6/intro1.js
@@ -0,0 +1,16 @@
+const fruit = ["apple", "banana", "cherry", "date", "elderberry"];
+for(let i = 0; i < fruit.length; i++) {
+  console.log(fruit[i]);
+}
+for(let i in fruits){
+    console.log('For in:',indexedDB, fruits[index])
+}
+
+let i = 0;
+while(i < fruit.length) {
+  console.log(fruit[i]);
+  i++;
+}
+for(let fruitItem of fruit) {
+  console.log(fruitItem);
+}
